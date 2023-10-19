f72ff75559ad890c7797b384b5ab49b5aee86089
Jose Giron <62405132+josegiron1@users.noreply.github.com>
2023-10-18 23:16:08 -0400

    remove api folder at the moment

--- src/app/api/user/route.ts
+++
@@ -1,8 +0,0 @@
-import { getAuth } from "@clerk/nextjs/server";
-import { NextApiRequest, NextApiResponse } from "next";
- 
-export async function POST(req: NextApiRequest) {
-  const { userId } = getAuth(req);
-  console.log(userId)
-return Response.json({ userId });
-}
